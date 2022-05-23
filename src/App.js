111f26085fb547cc6c1f31d6ab72177a501e905e
Grant <grant.pritchard@contactpartners.com>
2022-05-23 19:50:32 +0100

    trying to push the input into the object

--- src/App.js
+++ src/App.js
@@ -6,17 +6,22 @@ import React, { useState } from "react";
 function App() {
   //this is the starting state when the page loads
   const [taskList, setTaskList] = useState([
-    { task: "", check: false, },
+    { 
+      task: "", check: false, 
+    },
 
 ]);
 
 //the event adds a new task into state
 const handleTaskAdd = () => {
-  if(!taskList.some(t => t.task === "")) {
-    setTaskList([...taskList, { task: "", check: false, },])
-  } else {
-    alert("Please fill in the empty task")
-  }
+  let addTask = document.getElementById('addTask');
+  taskList.task.push(addTask.value)
+  setTaskList([...taskList, { task: "", check: false, },])
+  // if(!taskList.some(t => t.task === "")) {
+    
+  // } else {
+  //   alert("Please fill in the empty task")
+  // }
 }
 
 //the event removes the task selected by the button click
@@ -51,12 +56,15 @@ console.log(taskList)
         <label htmlFor="task">Tasks</label>
         {taskList.map((singleTask, index) => (
         <div key={index}>
-          <input name="task" type="text" id="task" required value={singleTask.task} onChange = {(e) => handleTaskChange(e, index, "task")}/>
-          <input name="check" type="checkbox" id="check" required value={singleTask.check} onChange = {(e) => handleTaskChange(e, index, "checkbox")}/>
+          <ul>
+            <li><label name="task" type="text" id="task" required value={singleTask.task} onChange = {(e) => handleTaskChange(e, index, "task")}>{singleTask.task}</label><input name="check" type="checkbox" id="check" required value={singleTask.check} onChange = {(e) => handleTaskChange(e, index, "checkbox")}/></li>
+          </ul>
+          
           <div>
             {taskList.length > 1 && <button type="button" onClick={() => handleTaskRemove(index)}>Remove Task</button>}
           </div>
           <div>
+            <input name="addTask" type="text" id="addTask"/>
             {taskList.length - 1 === index && <button type="button" onClick={handleTaskAdd}>Add Task</button>}
           </div>
         </div>
